224afee0cd29d9e8ffdc312c791f2ed42d99e1d3
Francois Laubscher <francoislaubscher@outlook.com>
2017-02-06 21:29:26 +0200

    error handling

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const imager = require('node-imager');
 const app = express();
 
 app.get('/', (req, res) => { res.redirect('https://github.com/fjlaubscher/node-imagehandler'); });
-app.get('/:dimensions/:url', function(req, res) {
+app.get('/:dimensions/*', function(req, res) {
   const dimensions = req.params.dimensions;
-  const url = req.params.url;
+  const url = req.params['0']; // this feels hacky
 
-  const parts = dimensions && dimensions.toLowerCase().split('x') || null;    
+  const parts = dimensions && dimensions.toLowerCase().split('x') || null;
   if (!parts) {
     res.send('Invalid dimensions. Please separate width and height as "<width>x<height>".');
-  } else if (parts.length != 2) {
+  }
+
+  const width = parseInt(parts[0]);
+  const height = parseInt(parts[1]);
+
+  if (isNaN(width) || isNaN(height)) {
     res.send('Invalid dimensions. Missing a height or width.');
   }
   
@@ -18,14 +23,12 @@ app.get('/:dimensions/:url', function(req, res) {
     res.send(`Invalid image url: ${url}`);
   }
 
-  const width = parseInt(parts[0]);
-  const height = parseInt(parts[1]);
-
-  const options = { format: 'c', width, height, url };
+  const imageUrl = new RegExp('http(s)?://', 'i').test(url) ? url : `http://${url}`;
+  const options = { format: 'c', width, height, url: imageUrl };
 
   imager.resizeWith(options, (contentType, imageBuffer) => {
     res.setHeader('Content-Type', contentType);
     res.send(imageBuffer);
   });
 });
-app.listen(3000);
+app.listen(3000, () => { console.log('Node imagehandler running on port 3000'); });
